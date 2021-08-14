c700e01ab51e290aabfbb6e9ca62db276381ba5e
saasproduction <saasproduction2021@outlook.de>
2021-08-14 22:57:01 +0200

    Chat

---
+++ server/src/Chat/index.ts
@@ -0,0 +1,52 @@
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { ChatResolver } from "./subscription";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const main = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [ChatResolver],
+      validate: false,
+    }),
+    subscriptions: {
+      path: "/subscriptions",
+      onConnect: () => {
+        console.log("Client connected for subscriptions");
+      },
+      onDisconnect: () => {
+        console.log("Client disconnected from subscriptions");
+      },
+    },
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+  apolloServer.installSubscriptionHandlers(httpServer);
+
+  httpServer.listen(process.env.PORT, () => {
+    console.log(
+      `Server ready at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`
+    );
+    console.log(
+      `Subscriptions ready at ws://localhost:${process.env.PORT}${apolloServer.subscriptionsPath}`
+    );
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});

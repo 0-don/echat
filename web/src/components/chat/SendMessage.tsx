2ce97d4ae2a739fd3e94672e1e7492952de031ba
saasproduction <saasproduction2021@outlook.de>
2021-08-16 22:44:15 +0200

    frontend chat

---
+++ web/src/components/chat/SendMessage.tsx
@@ -0,0 +1,44 @@
+import { useState, FC } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+const SEND_MESSAGE = gql`
+  mutation createChat($name: String!, $message: String!) {
+    createChat(name: $name, message: $message) {
+      id
+      name
+      message
+    }
+  }
+`;
+
+interface SendMessageProps {
+  name: string;
+}
+
+const SendMessage: FC<SendMessageProps> = ({ name }) => {
+  const [input, setInput] = useState<string>("");
+  const [sendMessage] = useMutation(SEND_MESSAGE);
+
+  const handleSend = () => {
+    sendMessage({ variables: { name: name, message: input } })
+      .then((data) => {
+        console.log(data);
+        setInput("");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        id="message"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={handleSend}>Send message</button>
+    </div>
+  );
+};
+
+export default SendMessage;

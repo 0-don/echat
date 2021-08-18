d2b1217eb7178980ad073316829dc4ab72d49d63
saasproduction <saasproduction2021@outlook.de>
2021-08-19 01:07:34 +0200

    fix

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   images: {
-    domains: ['static-cdn.jtvnw.net'],
+    domains: ["static-cdn.jtvnw.net"],
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+  },
+  target: "node",
+  browser: {
+    fs: false,
+    path: false,
+    os: false,
   },
 };

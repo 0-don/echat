798a13d195557663433775dcc2ad1989e89c198e
Don Cryptus <don.cryptus@gmail.com>
2021-09-13 22:37:10 +0200

    fix

--- web/src/components/Wrapper.tsx
+++ web/src/components/Wrapper.tsx
@@ -4,16 +4,18 @@ interface WrapperProps {
   navbar?: boolean;
   fluid?: boolean;
   className?: string;
+  scrollbar?: boolean;
 }
 
 export const Wrapper: React.FC<WrapperProps> = ({
   children,
   navbar = false,
   fluid = false,
+  scrollbar,
   className,
 }) => {
   const trueClassName = fluid
-    ? `${className} h-full overflow-hidden`
+    ? `${className} h-full`
     : `${className} container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`;
 
   return (
@@ -22,7 +24,9 @@ export const Wrapper: React.FC<WrapperProps> = ({
         <div className='flex-1 flex flex-col'>
           {navbar && <NavBar />}
 
-          <div className={trueClassName}>{children}</div>
+          <div className={`${trueClassName} ${scrollbar && 'overflow-hidden'}`}>
+            {children}
+          </div>
         </div>
       </div>
     </>

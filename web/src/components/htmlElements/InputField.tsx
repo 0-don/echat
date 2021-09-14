d2be48ec60312d0b9850eb8a907151f9f2d06162
Don Cryptus <don.cryptus@gmail.com>
2021-09-14 17:08:46 +0200

    user details

--- web/src/components/htmlElements/InputField.tsx
+++ web/src/components/htmlElements/InputField.tsx
@@ -45,7 +45,7 @@ export const InputField: React.FC<InputFieldProps> = ({
         />
       </div>
       {error && (
-        <span className='flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1'>
+        <span className='flex text- items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1'>
           {error}
         </span>
       )}

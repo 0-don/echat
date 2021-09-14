1fe4b3ad8d1022e4a60bf269a6d65334363787e4
Don Cryptus <don.cryptus@gmail.com>
2021-09-14 19:58:35 +0200

    oush

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -322,7 +322,7 @@ export type User = {
   type?: Maybe<Scalars['String']>;
   username: Scalars['String'];
   email: Scalars['String'];
-  lastOnline: Scalars['DateTime'];
+  lastOnline: Scalars['String'];
   description?: Maybe<Scalars['String']>;
   age?: Maybe<Scalars['DateTime']>;
   gender?: Maybe<Scalars['String']>;

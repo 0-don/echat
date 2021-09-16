22134135c7373536905b1471456900467b3b5af9
saasproduction <saasproduction2021@outlook.de>
2021-09-17 01:00:32 +0200

    fixed cursor

--- web/src/components/user/Tabs.tsx
+++ web/src/components/user/Tabs.tsx
@@ -1,9 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { Dispatch, SetStateAction } from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Dispatch, SetStateAction } from "react";
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(" ");
 }
 
 export type Tab = {
@@ -20,17 +19,20 @@ interface TabsProps {
 export const Tabs: React.FC<TabsProps> = ({ tabs, setTabs }) => {
   return (
     <div>
-      <div className='mt-5'>
-        <div className='border-b '>
-          <nav className='-mb-px flex space-x-8' aria-label='Tabs'>
+      <div className="mt-5">
+        <div className="border-b ">
+          <nav
+            className="-mb-px flex space-x-8 cursor-pointer "
+            aria-label="Tabs"
+          >
             {tabs.map(({ name, icon, current }) => (
               <div
                 key={name}
                 className={classNames(
-                  current ? 'border-purple text-purple border-b-2' : '',
-                  'text-white group inline-flex items-center py-4 px-1 font-medium text-sm hover:border-b-2 hover:border-purple-dark hover:text-purple-dark'
+                  current ? "border-purple text-purple border-b-2" : "",
+                  "text-white group inline-flex items-center py-4 px-1 font-medium text-sm hover:border-b-2 hover:border-purple-dark hover:text-purple-dark"
                 )}
-                aria-current={current ? 'page' : undefined}
+                aria-current={current ? "page" : undefined}
                 onClick={() =>
                   setTabs(
                     tabs.map((tab) =>
@@ -42,12 +44,12 @@ export const Tabs: React.FC<TabsProps> = ({ tabs, setTabs }) => {
                 }
               >
                 <FontAwesomeIcon
-                  size='1x'
+                  size="1x"
                   className={classNames(
                     current
-                      ? 'border-purple'
-                      : 'text-white group-hover:text-purple-dark',
-                    '-ml-0.5 mr-2 h-5 w-5'
+                      ? "border-purple"
+                      : "text-white group-hover:text-purple-dark",
+                    "-ml-0.5 mr-2 h-5 w-5"
                   )}
                   icon={icon as any}
                 />

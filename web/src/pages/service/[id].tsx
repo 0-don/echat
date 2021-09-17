5a66573f64b18a72f3ea01ac6211f0d2510c1882
saasproduction <saasproduction2021@outlook.de>
2021-09-17 02:01:15 +0200

    outsourced score

--- web/src/pages/service/[id].tsx
+++ web/src/pages/service/[id].tsx
@@ -10,8 +10,9 @@ import { getRandomBetween } from "src/utils";
 import gray from "/public/gray.png";
 import { OrderModal } from "src/components/order/OrderModal";
 
-import { SRLWrapper } from "simple-react-lightbox";
+
 import Servicedetailcard from "src/components/servicedetail/Servicedetailcard";
+import Score from "src/components/servicedetail/Score";
 const ServiceDetail: NextPage<{ id: number }> = ({ id }) => {
   const [bgImage, setBgImage] = useState<string | undefined>();
   const { data } = useGetUserServiceByIdQuery({
@@ -56,40 +57,7 @@ const ServiceDetail: NextPage<{ id: number }> = ({ id }) => {
             </div>
           </div>
           <div className=" flex flex-0 space-x-4 ">
-            <div className=" ">
-              <div className="inset-x-0 top-0 flex space-x-40 rounded-lg border  bg-white dark:bg-dark dark:text-white shadow px-4 py-5 sm:rounded-lg sm:p-6 mb-3 text-center ">
-                <div>
-                  <h1>Review Score</h1>
-                  <h2>5.0/5.0</h2>
-                </div>
-                <div>
-                  <h1>Served</h1>
-                  <h2>52</h2>
-                </div>
-                <div>
-                  <h1>Recommended</h1>
-                  <h2>51</h2>
-                </div>
-              </div>
-              <SRLWrapper>
-                <div
-                  style={{
-                    position: "relative",
-                    width: "100%",
-                    height: "350px",
-                  }}
-                >
-                  <Image
-                    placeholder="blur"
-                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMUrAcAAKcAkqLcIOsAAAAASUVORK5CYII="
-                    layout="fill"
-                    objectFit="cover"
-                    src={userService?.image ?? transparent.src}
-                  />
-                </div>
-              </SRLWrapper>
-            </div>
-
+            <Score id={id} />
             <div
               style={{
                 width: "400px",

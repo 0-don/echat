1e1376e2c9001f874ae51e1090ab2a7ee769b617
Don Cryptus <don.cryptus@gmail.com>
2021-09-18 00:00:02 +0200

    remove lightbox

--- web/src/components/user/Album.tsx
+++ web/src/components/user/Album.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { SRLWrapper } from 'simple-react-lightbox';
-import Image from 'next/image';
 import { GetUserQuery } from 'src/generated/graphql';
 import transparent from '/public/transparent.png';
+import { ImagePopup } from '../utils/ImagePopup';
 
 interface AlbumProps {
   data: GetUserQuery | undefined;
@@ -14,21 +13,19 @@ export const Album: React.FC<AlbumProps> = ({ data }) => {
   );
 
   return (
-    <SRLWrapper>
-      <div className='grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-5'>
-        {images?.map((image) => (
-          <a href={image.url} key={image.id}>
-            <Image
-              width={'100%'}
-              height={'100%'}
-              layout='responsive'
-              objectFit='cover'
-              className='rounded-xl'
-              src={image.url ?? transparent.src}
-            />
-          </a>
-        ))}
-      </div>
-    </SRLWrapper>
+    <div className='grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-5'>
+      {images?.map((image) => (
+        <div key={image.id}>
+          <ImagePopup
+            width={'100%'}
+            height={'100%'}
+            layout='responsive'
+            objectFit='cover'
+            className='rounded-xl'
+            src={image.url ?? transparent.src}
+          />
+        </div>
+      ))}
+    </div>
   );
 };
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,11 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  darkMode: "class",
-  mode: "jit",
+  darkMode: 'class',
+  mode: 'jit',
   purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
@@ -13,23 +13,23 @@ module.exports = {
       red: colors.red,
       purple: {
         // unkwnonw
-        light: "#85d7ff",
+        light: '#85d7ff',
         // Button Text
-        DEFAULT: "#9147ff",
+        DEFAULT: '#9147ff',
         // Button:Mouseover Text:hover Input:Border
-        dark: "#772ce8",
+        dark: '#772ce8',
       },
 
       lightGray: {
-        DEFAULT: "#5e5e60",
+        DEFAULT: '#5e5e60',
       },
       dark: {
         // Input:normal
-        light: "#464649",
+        light: '#464649',
         // containers
-        DEFAULT: "#18181b",
+        DEFAULT: '#18181b',
         // Background Input:Focus
-        dark: "#0e0e10",
+        dark: '#0e0e10',
       },
       black: colors.black,
       yellow: colors.yellow,

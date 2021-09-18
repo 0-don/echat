ab307cad7ea10b1a64400ac56b65a19410978389
saasproduction <saasproduction2021@outlook.de>
2021-09-18 22:56:35 +0200

    responsive

--- web/src/components/servicedetail/Servicedetailcard.tsx
+++ web/src/components/servicedetail/Servicedetailcard.tsx
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 import { useRouter } from "next/router";
-=======
-import { useRouter } from 'next/router';
->>>>>>> master
 
-import gray from '/public/gray.png';
-import React from 'react';
-import Image from 'next/image';
-import dayjs from 'dayjs';
-import transparent from '/public/transparent.png';
-import { useGetUserServiceByIdQuery } from 'src/generated/graphql';
-import { NextPage } from 'next';
+import gray from "/public/gray.png";
+import React from "react";
+import Image from "next/image";
+import dayjs from "dayjs";
+import transparent from "/public/transparent.png";
+import { useGetUserServiceByIdQuery } from "src/generated/graphql";
+import { NextPage } from "next";
 
 const Servicedetailcard: NextPage<{ id: number }> = ({ id }) => {
   const { data } = useGetUserServiceByIdQuery({
@@ -22,56 +18,49 @@ const Servicedetailcard: NextPage<{ id: number }> = ({ id }) => {
   const router = useRouter();
   return (
     <div>
-      {' '}
-      <div
-        style={{
-          width: '400px',
-          height: '550px',
-        }}
-        className=' items-center flex flex-col bg-white dark:bg-dark dark:text-white shadow px-4 py-4 sm:rounded-lg sm:p-6 mb-5'
-      >
+      <div className=" w-[350px] h-[500px] sm:w-[400px] sm:h-[550px] items-center flex flex-col bg-white dark:bg-dark dark:text-white shadow px-4 py-4  sm:p-6 mb-5">
         <div
           style={{
-            position: 'relative',
-            width: '300px',
-            height: '300px',
-            maxWidth: '400px',
-            maxHeight: '300px',
+            position: "relative",
+            width: "300px",
+            height: "300px",
+            maxWidth: "400px",
+            maxHeight: "300px",
           }}
         >
           <Image
-            placeholder='blur'
-            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMUrAcAAKcAkqLcIOsAAAAASUVORK5CYII='
-            layout='fill'
-            objectFit='cover'
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMUrAcAAKcAkqLcIOsAAAAASUVORK5CYII="
+            layout="fill"
+            objectFit="cover"
             src={
-              user?.images?.find((image) => image.type == 'profile')?.url ??
+              user?.images?.find((image) => image.type == "profile")?.url ??
               transparent.src
             }
           />
         </div>
 
-        <div className='flex  space-x-10 text-center'>
+        <div className="flex  space-x-10 text-center">
           <div
             onClick={() => router.push(`/user/${user?.id}`)}
-            className='text-3xl cursor-pointer text-purple-dark '
+            className="text-3xl cursor-pointer text-purple-dark "
           >
             {user?.username}
           </div>
-          {dayjs(new Date()).diff(user?.lastOnline, 'day') * -1 < 2 ? (
-            <div className='bg-green-500 h-8 w-8 rounded-full mr-1'></div>
+          {dayjs(new Date()).diff(user?.lastOnline, "day") * -1 < 2 ? (
+            <div className="bg-green-500 h-8 w-8 rounded-full mr-1"></div>
           ) : (
-            <div className='bg-red-500 h-8 w-8 rounded-full mr-1'></div>
+            <div className="bg-red-500 h-8 w-8 rounded-full mr-1"></div>
           )}
         </div>
-        <div className='flex-col container '>
-          <div className='flex flex-0 space-x-2  '>
-            <div className='text-lg'>Language:</div>
+        <div className="flex-col container ">
+          <div className="flex flex-0 space-x-2  ">
+            <div className="text-lg">Language:</div>
             {languages?.map((language) => {
               return (
                 <div
                   key={language.id}
-                  className='dark:text-white text-black text-lg '
+                  className="dark:text-white text-black text-lg "
                 >
                   {language.name}
                 </div>
@@ -79,26 +68,26 @@ const Servicedetailcard: NextPage<{ id: number }> = ({ id }) => {
             })}
           </div>
 
-          <div className='text-lg text-center'>Services</div>
+          <div className="text-lg text-center">Services</div>
           {/* <div className="flex flex-0  space-x-2  "> */}
-          <div className='flex flex-0  space-x-2  '>
+          <div className="flex flex-0  space-x-2  ">
             {services?.map((service) => {
               return (
                 <div
                   key={service.id}
                   onClick={() => router.push(`/service/${service.id}`)}
                   style={{
-                    position: 'relative',
-                    width: '50px',
-                    height: '50px',
+                    position: "relative",
+                    width: "50px",
+                    height: "50px",
                   }}
                 >
                   <Image
-                    placeholder='blur'
-                    blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMUrAcAAKcAkqLcIOsAAAAASUVORK5CYII='
-                    layout='fill'
-                    objectFit='cover'
-                    src={service ? '' + service?.service?.boxArtUrl : gray.src}
+                    placeholder="blur"
+                    blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mMUrAcAAKcAkqLcIOsAAAAASUVORK5CYII="
+                    layout="fill"
+                    objectFit="cover"
+                    src={service ? "" + service?.service?.boxArtUrl : gray.src}
                   />
                 </div>
               );

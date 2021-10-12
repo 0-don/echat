b1de72402eb4cd20de0cc775782fd9ad424eec44
Don Cryptus <don.cryptus@gmail.com>
2021-10-12 23:00:51 +0200

    Chat Resolvers & Relations

---
+++ server/src/entity/Participant.ts
@@ -0,0 +1,45 @@
+import { Lazy } from '../utils';
+import { ObjectType, Field, Int } from 'type-graphql';
+import { TypeormLoader } from 'type-graphql-dataloader';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './User';
+import { Room } from './Room';
+
+@ObjectType()
+@Entity()
+export class Participant extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => Int)
+  @Column()
+  roomId: number;
+
+  // ROOM
+  @Field(() => Room)
+  @ManyToOne(() => Room, (room) => room.participants, {
+    lazy: true,
+    onDelete: 'CASCADE',
+  })
+  @TypeormLoader()
+  room: Lazy<Room>;
+
+  @Field(() => Int)
+  @Column()
+  userId: number;
+
+  // USER
+  @Field(() => User)
+  @ManyToOne(() => User, (user) => user.participants, {
+    lazy: true,
+  })
+  @TypeormLoader()
+  user: Lazy<User>;
+}
--- server/src/utils/types/MyContext.ts
+++ server/src/utils/types/MyContext.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
+import { Session } from 'express-session';
 
 export type MyContext = {
-  // @ts-ignore
-  req: Request & { session: Express.Session };
+  req: Request & { session: Session & { userId: number } };
   res: Response;
 };

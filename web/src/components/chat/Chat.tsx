b1169bf11c94980ec547a736576a81772a408e14
Don Cryptus <don.cryptus@gmail.com>
2021-10-15 17:43:57 +0200

    chat fix

--- web/src/components/chat/Chat.tsx
+++ web/src/components/chat/Chat.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { useGetRoomsQuery } from 'src/generated/graphql';
 import { Messages } from './Messages';
@@ -11,23 +12,26 @@ export const Chat: React.FC = () => {
   if (error) return <p>`Error! ${error.message}`</p>;
 
   return (
-    <>
-      <div className='text-white fixed bottom-0 right-0 z-10 bg-dark-light p-5 rounded-lg mr-10 mb-10'>
-        <div className='flex space-x-5'>
-          <Rooms
-            data={data}
-            currentChannel={currentChannel}
-            setCurrentChannel={setCurrentChannel}
-          />
+    <div className='fixed bottom-0 right-0 z-10 mr-10 mb-10 flex flex-col items-end'>
+      <div className='flex space-x-5 text-white bg-dark-light p-5 rounded-lg'>
+        <Rooms
+          data={data}
+          currentChannel={currentChannel}
+          setCurrentChannel={setCurrentChannel}
+        />
 
-          <Messages
-            data={data}
-            currentChannel={currentChannel}
-            setCurrentChannel={setCurrentChannel}
-            subscribeToMore={subscribeToMore}
-          />
-        </div>
+        <Messages
+          data={data}
+          currentChannel={currentChannel}
+          setCurrentChannel={setCurrentChannel}
+          subscribeToMore={subscribeToMore}
+        />
       </div>
-    </>
+      <FontAwesomeIcon
+        size='4x'
+        className='dark:text-white text-white bg-purple dark:bg-purple dark:hover:bg-purple-dark hover:bg-purple-dark p-2.5 rounded-full '
+        icon='comment-dots'
+      />
+    </div>
   );
 };
